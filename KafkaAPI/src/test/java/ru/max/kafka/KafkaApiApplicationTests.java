7b1feff790d94cc224e209f05d9db4a23c02a844
m.alekseev <m.alekseev@platformaofd.ru>
2022-06-28 15:57:10 +0300

    Kafka api

---
+++ KafkaAPI/src/test/java/ru/max/kafka/KafkaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.max.kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
